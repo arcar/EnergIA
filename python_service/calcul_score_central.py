782654038a3df7757e340921466b2fe3ec1eb0ef
Mario <mariomorkos98@gmail.com>
2026-08-04 16:52:45 +0200

    <Fix> ajouter les calculs des paramètres de score des centrales

--- python_service/calcul_score_central.py
+++ python_service/calcul_score_central.py
@@ -8,26 +8,51 @@ def extract_data():
     with open(fichier_data, "r", encoding="utf-8") as json_file:
         return json.load(json_file)
         
+def distance_km(edges):
+    return edges["geodesic_distance_km"]
+
+def loss_percent(edges):
+    return edges["estimated_loss_percent"]
 
 
-def calcul_puissance_disponible(centrale):
-    return centrale["simulation"]["soft_upper_bound_mw"] - centrale["simulation"]["initial_output_mw"]
+def final_load_ratio(centrale, scenario):
+    return (centrale["simulation"]["initial_output_mw"] + scenario["additional_demand_mw"]) / centrale["simulation"]["soft_upper_bound_mw"]
 
-def taux_saturation(centrale):
-    return centrale["simulation"]["initial_load_ratio"]
+def technical_penalty(centrale):
+    penalty = 0
+    for reactor in centrale["reactors"]:
+        if (reactor["status"] == "in_operation"):
+            penalty += 0
+        elif (reactor["status"] == "maintenance"):
+            penalty += 10
+        else:
+            penalty += 100
+    return penalty
 
-def verifier_disponibilite(centrale):
-    return centrale["simulation"]["available"]
+def regional_priority_bonus_if_local(centrale, scenario):
+    if (centrale["region_id"] == scenario["region_id"]):
+        return 50
+    else:
+        return 0
+    
 
-def metriques_centrales(donnees):
+
+def donnees_scores(donnees, scenario):
     resultats = []
+
+    for edges in donnees["plant_edges"]:
+        resultats.append({
+            "distance_km": distance_km(edges),
+            "loss_percent": loss_percent(edges)
+        })
 
     for centrale in donnees["plants"]:
         resultats.append({
             "id": centrale["id"],
-            "puissance_disponible": calcul_puissance_disponible(centrale),
-            "taux_saturation": taux_saturation(centrale),
-            "disponible": verifier_disponibilite(centrale)
+            "final_load_ratio" : final_load_ratio(centrale, scenario),
+            "technical_penalty" : technical_penalty(centrale),
+            "regional_priority_bonus" : regional_priority_bonus_if_local(centrale, scenario)
+
         })
 
     return resultats

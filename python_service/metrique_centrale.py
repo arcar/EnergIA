e7c6f0af5efc27fc7994f0f28d4f37c751f4fc2d
Mario <mariomorkos98@gmail.com>
2026-08-05 17:07:36 +0200

    ajout du calcule des capacités et la repartition locale

---
+++ python_service/metrique_centrale.py
@@ -0,0 +1,35 @@
+from calcul_score_central import extract_data
+
+def get_puissance_disponible(centrale):
+    return (centrale["simulation"]["soft_upper_bound_mw"] - centrale["simulation"]["initial_output_mw"])
+
+def get_centrale_disponible(centrale):
+    return centrale["simulation"]["available"]
+
+def get_taux_saturation(centrale):
+    return centrale["simulation"]["soft_upper_bound_ratio"]
+
+def get_nom_region(centrale):
+    return centrale["location"]["region_name"]
+
+def get_central_id(centrale):
+    return centrale["id"]
+
+def get_metrique_centrale():
+    donnees = extract_data()
+    metriques = []
+
+    for centrale in donnees["plants"]:
+
+        metriques.append({
+            "puissance_disponible" : get_puissance_disponible(centrale),
+            "taux_saturation": get_taux_saturation(centrale),
+            "disponible": get_centrale_disponible(centrale),
+            "region": get_nom_region(centrale),
+            "central_id": get_central_id(centrale)
+        })
+
+    metriques.sort(key=lambda x: x["region"])
+    return metriques
+
+print(get_metrique_centrale())

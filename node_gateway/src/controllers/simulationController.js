25efcc532df123a2e44fe4ad3009abf1dc81ca18
yassine Bessaim <yassinebessaim@gmail.com>
2026-08-06 09:15:13 +0200

    log et message

--- node_gateway/src/controllers/simulationController.js
+++ node_gateway/src/controllers/simulationController.js
@@ -3,16 +3,30 @@ const pythonService = require("../services/pythonService");
 
 async function simulation(req, res) {
 
+    console.log("Simulation demandée");
+    console.log("Région :", req.body.region);
+    console.log("Augmentation :", req.body.augmentation, "MW");
+
     try {
 
         const resultat = await pythonService.simulate(req.body);
 
-        res.json(resultat);
+        console.log("Simulation terminée");
+
+        res.json({
+            success: true,
+            data: resultat
+        });
 
     } catch(error) {
 
+        console.error("Erreur simulation :", error.message);
+
         res.status(500).json({
-            message: error.message
+            success: false,
+            message: "Impossible d'effectuer la simulation",
+            error: error.message,
+            status: 500
         });
 
     }

6b38ab88d06b8a42cbf92428e9fe2505889848f9
yassine Bessaim <yassinebessaim@gmail.com>
2026-08-25 10:40:42 +0200

    lecture fichié json

--- python_service/prod_non_pilotable.py
+++ python_service/prod_non_pilotable.py
@@ -4,6 +4,9 @@ import json
 parent = os.path.dirname(os.path.abspath(__file__))
 parc_non_pilotable_data = os.path.join(parent, "data", "energia-production-non-pilotable.json")
 
+with open(parc_non_pilotable_data, "r", encoding="utf-8") as fichier:
+    data = json.load(fichier)
+
 #-----------------------------------------
 # SOLAIRE
 #------------------------------------------

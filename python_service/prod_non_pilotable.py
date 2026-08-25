5f314cf8b1a59ced51207eecb006a07834c1d160
yassine Bessaim <yassinebessaim@gmail.com>
2026-08-25 10:38:24 +0200

    modif variable

--- python_service/prod_non_pilotable.py
+++ python_service/prod_non_pilotable.py
@@ -2,7 +2,7 @@ import os
 import json
 
 parent = os.path.dirname(os.path.abspath(__file__))
-parc_nucleaire_data = os.path.join(parent, "data", "energia-production-non-pilotable.json")
+parc_non_pilotable_data = os.path.join(parent, "data", "energia-production-non-pilotable.json")
 
 #-----------------------------------------
 # SOLAIRE

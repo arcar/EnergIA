d54137b3c74bcefc6c01e1917bf8fb452dfb2141
yassine Bessaim <yassinebessaim@gmail.com>
2026-09-02 16:17:15 +0200

    ajout de la route dashboard dans le gateway

--- node_gateway/src/index.js
+++ node_gateway/src/index.js
@@ -8,9 +8,11 @@ require("dotenv").config();
 
 const plantRoutes = require("./routes/plantRoutes");
 const simulationRoutes = require("./routes/simulationRoutes")
+const dashboardRoutes=require("./routes/dashboardRoutes");
 
 app.use("/plants", plantRoutes); 
 app.use("/simulation", simulationRoutes)
+app.use("/dashboard",dashboardRoutes)
 
 app.listen(port, () => {
   console.log(`Application à l'écoute sur le port ${port}!`);

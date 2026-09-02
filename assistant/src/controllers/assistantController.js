b5856237ff3ac56c9454951c8f2862f80983ef70
Cédric Michaud <arcar13@gmail.com>
2026-09-02 09:41:07 +0200

    <Feat> assistantService presque fonctionnel

--- assistant/src/controllers/assistantController.js
+++ assistant/src/controllers/assistantController.js
@@ -2,23 +2,29 @@ const assistantService = require("../services/assistantService");
 
 const ollamaMessage = async (req, res) => {
     try {
-        const question = req.query.search;
-        if(!question){
+        const question = req.body.prompt;
+
+        if (!question) {
             return res.status(400).json({
                 error: "Missing Question!"
             });
         }
-        const reply = await assistantService.generateAnswer(question);
-        res.status(200).json(reply);
-        return 
-    }catch (error){
-            console.error(error);
 
-    return res.status(500).json({
-        error: error.message,
-        stack: error.stack
-    });
-        }
-}
+        const answer = await assistantService.generateAnswer(question);
 
-module.exports = {ollamaMessage};
+        return res.status(200).json({
+            response: answer
+        });
+
+    } catch (error) {
+        console.error("Erreur assistant :", error);
+
+        return res.status(500).json({
+            error: error.message
+        });
+    }
+};
+
+module.exports = {
+    ollamaMessage
+};
--- assistant/src/services/ollamaService.js
+++ assistant/src/services/ollamaService.js
@@ -1,14 +1,42 @@
-const { Ollama } = require('ollama');
+const axios = require("axios");
 
-const ollamaClient = new Ollama({
-    host: process.env.OLLAMA_URL
-});
-async function askOllama(prompt) {
-    const response = await ollamaClient.chat({
-        model: 'qwen2.5:3b',
-        messages : prompt
-    })
-    return response.message.content;
+async function askLLM(prompt) {
+    console.log("➡️ Appel Ollama...");
+    console.log("Prompt :", prompt);
+
+    try {
+        const response = await axios.post(
+            "http://127.0.0.1:11434/api/generate",
+            {
+                model: "qwen2.5:3b",
+                prompt: prompt,
+                stream: false
+            },
+            {
+                timeout: 120000
+            }
+        );
+
+        console.log("✅ Réponse Ollama reçue");
+        console.log("Réponse :", response.data.response);
+
+        return response.data.response;
+
+    } catch (error) {
+        console.error("❌ ERREUR OLLAMA");
+
+        if (error.response) {
+            console.error("Status :", error.response.status);
+            console.error("Data :", error.response.data);
+        } else {
+            console.error("Message :", error.message);
+            console.error("Code :", error.code);
+        }
+
+        throw error;
+    }
 }
 
-module.exports = {askOllama};
+module.exports = {
+    askLLM
+};

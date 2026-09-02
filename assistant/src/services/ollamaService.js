188d851f4857763450f3c4cf08e563397e9619cc
Cédric Michaud <arcar13@gmail.com>
2026-09-02 14:43:46 +0200

    <Fix> ollamaServicz.js

--- assistant/src/services/ollamaService.js
+++ assistant/src/services/ollamaService.js
@@ -1,9 +1,4 @@
 const axios = require("axios");
-const { Ollama } = require('ollama');
-
-const ollamaClient = new Ollama({
-    host: process.env.OLLAMA_URL
-});
 
 async function askLLM(prompt) {
     console.log("➡️ Appel Ollama...");
@@ -11,33 +6,23 @@ async function askLLM(prompt) {
 
     try {
         const response = await axios.post(
-            ollamaClient.chat,
+            "http://host.docker.internal:11434/api/generate",
             {
                 model: "qwen2.5:3b",
-                messages : prompt,
+                prompt: prompt,
                 stream: false
-            },
-            {
-                timeout: 120000
             }
         );
 
-        console.log("✅ Réponse Ollama reçue");
-        console.log("Réponse :", response.data.response);
+        console.log("📦 STATUS :", response.status);
+        console.log("📦 DATA BRUTE :", JSON.stringify(response.data, null, 2));
 
         return response.data.response;
 
     } catch (error) {
         console.error("❌ ERREUR OLLAMA");
-
-        if (error.response) {
-            console.error("Status :", error.response.status);
-            console.error("Data :", error.response.data);
-        } else {
-            console.error("Message :", error.message);
-            console.error("Code :", error.code);
-        }
-
+        console.error("Message :", error.message);
+        console.error("Code :", error.code);
         throw error;
     }
 }

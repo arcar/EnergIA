0c2e95a47d41cab34704027eda075d0d31680164
Mario <mariomorkos98@gmail.com>
2026-09-03 12:01:28 +0200

    ajout de tools.js

---
+++ assistant/src/services/tools.js
@@ -0,0 +1,14 @@
+export const toolBox = [
+    {
+        "type" : "function",
+        "function" : {
+            "name" : "get_centrales",
+            "description": "Récupère la liste des centrales nucléaires françaises. À utiliser quand l'utilisateur demande la liste, le nombre, ou les informations sur les centrales nucléaires, que ce soit pour toute la France ou une région spécifique.",
+            "parameters" : {
+                "type": "object",
+                "properties": {},
+                "required": ["all_plants"]
+            }
+        }
+    }
+]

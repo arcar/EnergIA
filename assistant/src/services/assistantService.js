c7be85ed5696cdff33e68551e0053d0810a756be
Mario <mariomorkos98@gmail.com>
2026-09-04 09:28:25 +0200

    local save

--- assistant/src/services/assistantService.js
+++ assistant/src/services/assistantService.js
@@ -1,39 +1,36 @@
 const ollamaService = require("./ollamaService");
-
-let buffer = "";
-
-process.stdin.on("data", (chunk) => {
-    buffer += chunk;
-    const lines = buffer.split("\n");
-    buffer = lines.pop();
-
-    for (const line of lines){
-        if(!line.trim()) continue;
-        handeLine(line);
-    }
-})
-
-async function handeLine(line){
-    let request;
-    try {
-        request = JSON.parse(line)
-    } catch (e) {
-        console.error("Erreur de parsing JSON:", e);
-        return;
-    }
-
-    
-}
+const axios = require("axios");
 
 
 async function generateAnswer(question) {
     console.log("Question reçue :", question);
 
-    const answer = await ollamaService.askLLM(question);
+    const result = await ollamaService.askLLM(question);
+
+    switch (result.action){
+        case "GET_PLANTS":
+            try {
+            
+                    const response = await axios.get(`${process.env.PYTHON_SERVICE_URL}/plants`);
+            
+                    return response.data;
+            
+                } catch (error) {
+            
+                    console.log(error.message);
+            
+                    throw new Error(
+                        "Impossible de contacter l'API Python"
+                    );
+            
+                }
+            break;
+        case "GET_CONSO":
+
+            break;
+    }
 
     console.log("Réponse Ollama reçue");
-
-    return answer;
 }
 
 module.exports = {

1e018102929cc575fcb408a8f2a96359835b9402
yassine Bessaim <yassinebessaim@gmail.com>
2026-09-10 15:53:28 +0200

    création service d'historique

---
+++ frontend/src/app/services/history.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from '@angular/core/testing';
+import { History } from './history';
+
+describe('History', () => {
+  let service: History;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(History);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
